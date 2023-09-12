44804ac8a0f2b824850f263e8157ffe99d78b76e
Luis Vides <luis.rph@protonmail.com>
2023-09-12 12:10:52 -0300

    Se agrega soporte para cors

--- src/my_api/cmd/my_api/main.go
+++ src/my_api/cmd/my_api/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"my_api/internal/handler"
 	"net/http"
+
+	"github.com/gorilla/handlers"
 )
 
 func main() {
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
 	http.HandleFunc("/datos-persona", handler.DatosPersonaHandler)
-	http.ListenAndServe(":8080", nil)
+
+	http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(http.DefaultServeMux))
 }
--- src/my_api/go.mod
+++ src/my_api/go.mod
@@ -1,3 +1,7 @@
 module my_api
 
 go 1.21.0
+
+require github.com/gorilla/handlers v1.5.1
+
+require github.com/felixge/httpsnoop v1.0.1 // indirect
